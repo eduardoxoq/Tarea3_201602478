4a9d4e09761ea72747f37f46f72a69c89b304db5
Eduardo <Eduardo@192.168.0.15>
2017-08-07 00:28:06 -0600

    Tarea3

---
+++ src/tarea3/Tarea3.java
@@ -0,0 +1,22 @@
+
+package tarea3;
+/**
+ *
+ * @author Eduardo
+ */
+public class Tarea3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Tarea 3");
+        System.out.println("");
+        System.out.println("201602478");
+        System.out.println("1. Usuarios");
+        System.out.println("2. Palabras Palindromas");
+        System.out.println("3. Salir");
+        
+    }
+    
+}
